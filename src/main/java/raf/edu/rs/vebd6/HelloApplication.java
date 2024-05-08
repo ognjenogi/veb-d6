ccfee999d2e3c2b00fd710023f67af1e5c13d127
Ognjen Nikolic <onikolic12322rn@raf.rs>
2024-05-08 18:51:39 +0200

    little fix

--- src/main/java/raf/edu/rs/vebd6/HelloApplication.java
+++ src/main/java/raf/edu/rs/vebd6/HelloApplication.java
@@ -1,14 +1,13 @@
 package raf.edu.rs.vebd6;
 
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
 
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
-import raf.edu.rs.vebd6.repository.comment.CommentRepImpl;
+import raf.edu.rs.vebd6.repository.comment.CommentRepPostgresSql;
 import raf.edu.rs.vebd6.repository.comment.CommentRepository;
-import raf.edu.rs.vebd6.repository.post.PostRepImpl;
+import raf.edu.rs.vebd6.repository.post.PostRepPostgresSql;
 import raf.edu.rs.vebd6.repository.post.PostRepository;
 import raf.edu.rs.vebd6.repository.user.UserRepImpl;
 import raf.edu.rs.vebd6.repository.user.UserRepository;
@@ -28,8 +27,8 @@ public class HelloApplication extends ResourceConfig {
         AbstractBinder binder = new AbstractBinder() {
             @Override
             protected void configure() {
-                this.bind(PostRepImpl.class).to(PostRepository.class).in(Singleton.class);
-                this.bind(CommentRepImpl.class).to(CommentRepository.class).in(Singleton.class);
+                this.bind(PostRepPostgresSql.class).to(PostRepository.class).in(Singleton.class);
+                this.bind(CommentRepPostgresSql.class).to(CommentRepository.class).in(Singleton.class);
                 this.bind(UserRepImpl.class).to(UserRepository.class).in(Singleton.class);
 
                 this.bindAsContract(UserService.class);
